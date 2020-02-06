ad60e7a97101d7be07f2c9935da4860181441a7f
Emanuel Zhupa <emanuelzhupa@live.com>
2020-02-06 21:30:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+To spit out code on bundle.js file run this command "npm run build" /'production'
+To run the development server "npm run serve" /'development
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import { styleBody, addTitle } from './dom';
+// import users, { getPremUsers } from './data';
+
+// console.log('Index File');
+// addTitle('test');
+
+// const premiumUsers = getPremUsers(users);
+// console.log(premiumUsers);
+
+// console.log(users);
