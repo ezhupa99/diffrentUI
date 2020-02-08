43b8f72d8799f7dbdc96a44ae2972814494bb8ce
Emanuel Zhupa <ezhupa99@gmail.com>
2020-02-08 03:27:13 +0100

    importing tabs.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import Tooltip from './ui/tooltip';
 import Dropdown from './ui/dropdown';
+import Tabs from './ui/tabs';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-const dropdown = new Dropdown(document.querySelector('.dropdown'));
 tooltip.init();
+
+const dropdown = new Dropdown(document.querySelector('.dropdown'));
 dropdown.init();
+
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
