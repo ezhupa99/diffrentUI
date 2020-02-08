dece13ca3eebcc551b55eb8baf792f2b40c45fb9
Emanuel Zhupa <ezhupa99@gmail.com>
2020-02-08 04:05:20 +0100

    add snackbar notification

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './ui/tooltip';
 import Dropdown from './ui/dropdown';
 import Tabs from './ui/tabs';
+import Snackbar from './ui/snackbar';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
@@ -10,3 +11,12 @@ dropdown.init();
 
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+	snackbar.show('You clicked me :)');
+});
