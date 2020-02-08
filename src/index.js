0a591af9f2b1cd13420750ff0e085b9cef3d0e70
Emanuel Zhupa <ezhupa99@gmail.com>
2020-02-08 03:06:17 +0100

    import dropdown to index js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Tooltip from './ui/tooltip';
+import Dropdown from './ui/dropdown';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
+const dropdown = new Dropdown(document.querySelector('.dropdown'));
 tooltip.init();
+dropdown.init();
